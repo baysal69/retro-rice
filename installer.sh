694e9a5db432400888fbf77a2dc4320f06507e8c
baysal69 <aissiwassim@gmail.com>
2024-10-13 20:42:59 +0100

    attempting chicago installer

--- installer.sh
+++ installer.sh
@@ -1,7 +1,19 @@
 echo "cloning chicago95"
 sleep 3
-git config --global http.version HTTP/1.1
+sudo git config --global http.version HTTP/1.1
 git clone https://github.com/grassmunk/Chicago95 --depth 1
 cd Chicago95
 git fetch --unshallow
-./installer.py
+cd ..
+sudo cp -r conky.conf /etc/conky/
+mkdir ~/.config/autostart
+sudo cp -r conky.desktop ~/.config/autostart/
+sudo cp -r punpun /etc/
+sudo cp -r config.conf ~/.config/neofetch/
+sudo cp -r neofetch /usr/bin/
+
+
+cd /usr/share/xsessions
+sudo mv pop.desktop pop.desktop1
+sudo mv xfce.desktop pop.desktop
+
