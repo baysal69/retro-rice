8f897d0e316f78fa9f84b06cfbba060fb1c92e59
baysal69 <aissiwassim@gmail.com>
2024-10-13 23:08:34 +0100

    trying installer

--- dependencies/dependencies-installer.sh
+++ dependencies/dependencies-installer.sh
@@ -1,35 +1,35 @@
 echo "starting to install dependencies"
 sleep 2
-echo "-y installing xfce 4"
+echo "installing xfce 4"
 sleep 3
 sudo apt-get -y install xfce4
-echo "-y installing gtk3"
+echo "installing gtk3"
 sleep 3
 sudo apt-get -y install gtk+3.0
-echo "-y installing gtk2-ingines-pixbuf"
+echo "installing gtk2-ingines-pixbuf"
 sleep 3
 sudo apt-get -y install gtk2-engines-pixbuf
-echo "-y installing xfce panel profiles"
+echo "installing xfce panel profiles"
 sleep 3
 sudo apt-get -y install xfce4-panel-profiles
-echo "-y installing whisker"
+echo "installing whisker"
 sleep 3
 sudo apt-get -y install xfce4-whiskermenu-plugin
-echo "-y installing conky"
+echo "installing conky"
 sleep 3
 sudo apt -y install conky
-echo "-y installing gtk3"
+echo "installing gtk3"
 sleep 3
 sudo apt-get -y install gtk3
-echo "-y installing libkeybinder3"
+echo "installing libkeybinder3"
 sleep 3
 sudo apt-get -y install libkeybinder-3.0
-echo "-y installing rust"
+echo "installing rust"
 sleep 3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo "-y installing cargo"
+echo "installing cargo"
 sleep 3
 sudo apt -y install cargo
-echo "-y installing neofetch"
+echo "installing neofetch"
 sleep 3
 sudo apt -y install neofetch
--- installer.sh
+++ installer.sh
@@ -1,9 +1,21 @@
+echo "installing dependencies"
+sleep 2
+sudo chmod 777 dependencies/dependencies-installer.sh
+./dependencies/dependencies-installer.sh
 echo "cloning chicago95"
 sleep 3
 sudo git config --global http.version HTTP/1.1
 git clone https://github.com/grassmunk/Chicago95 --depth 1
 cd Chicago95
 git fetch --unshallow
+cd ..
+sleep 3
+echo "cloning findex"
+git clone https://github.com/mdgaziur/findex
+echo "installing findex"
+cd findex
+sudo chmod 777 installer.sh
+./installer.sh
 cd ..
 echo "configuring conky"
 sudo cp -r conky.conf /etc/conky/
@@ -12,11 +24,12 @@ mkdir ~/.config/autostart
 sudo cp -r conky.desktop ~/.config/autostart/
 echo "configuring neofetch"
 sudo cp -r punpun /etc/
+echo "copying configs"
 sudo cp -r config.conf ~/.config/neofetch/
 sudo cp -r neofetch /usr/bin/
 sudo cp -r gtk.css ~/.config/gtk-3.0/
 echo "aliasing neofetch"
-sudo alias neofetch='neofetch --ascii_color 3 --source /etc/punpun'
+sudo alias neofetch='neofetch --ascii_colors 3 --source /etc/punpun'
 
 
 echo "logging off to refresh"
