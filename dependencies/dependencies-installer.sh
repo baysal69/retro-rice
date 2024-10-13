3a498fd13a0f91ad5af3b5d327e64b2d92520d23
baysal69 <aissiwassim@gmail.com>
2024-10-13 20:01:13 +0100

    attempting chicago installer

--- dependencies/dependencies-installer.sh
+++ dependencies/dependencies-installer.sh
@@ -6,30 +6,30 @@ sudo apt-get install xfce4
 echo "installing gtk3"
 sleep 3
 sudo apt-get install gtk+3.0
-echo "gtk2-ingines-pixbuf"
+echo "installing gtk2-ingines-pixbuf"
 sleep 3
 sudo apt-get install gtk2-engines-pixbuf
 echo "installing xfce panel profiles"
 sleep 3
-sudo apt0get install xfce4-panel-profiles
-echo "install whisker"
+sudo apt-get install xfce4-panel-profiles
+echo "installing whisker"
 sleep 3
 sudo apt-get install xfce4-whiskermenu-plugin
-echo "install conky"
+echo "installing conky"
 sleep 3
 sudo apt install conky
-echo "install gtk3"
+echo "installing gtk3"
 sleep 3
 sudo apt-get install gtk3
-echo "install libkeybinder3"
+echo "installing libkeybinder3"
 sleep 3
 sudo apt-get install libkeybinder-3.0
-echo "install rust"
+echo "installing rust"
 sleep 3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo "install cargo"
+echo "installing cargo"
 sleep 3
 sudo apt install cargo
-echo "install neofetch"
+echo "installing neofetch"
 sleep 3
 sudo apt install neofetch
---
+++ installer.sh
@@ -0,0 +1,5 @@
+echo "cloning chicago95"
+sleep 3
+git clone https://github.com/grassmunk/Chicago95
+cd Chicago95
+./installer.py
