ce81f182d25a6134371d3041f82c0994cba9339a
baysal69 <aissiwassim@gmail.com>
2024-10-13 18:33:19 +0100

    dependencies installer

---
+++ dependencies/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get install xfce4
+sudo apt-get install gtk+3.0
+sudo apt-get install gtk2-engines-pixbuf
+sudo apt0get install xfce4-panel-profiles
+sudo apt-get install xfce4-whiskermenu-plugin
+sudo apt install conky
+sudo apt-get install gtk3
+sudo apt-get install libkeybinder-3.0
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install cargo
+sudo apt install neofetch
